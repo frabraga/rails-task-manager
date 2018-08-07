ff2ba6009e2a1d88fef7f538dcc72b940f258d0b
Francisco Braga <franciscobragafernandes@gmail.com>
2018-08-07 13:48:01 -0300

    added edit and update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,7 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
-      end
+  end
 
   def show
     @task = Task.find(params[:id])
@@ -16,6 +16,20 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.save
     redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+
   end
 
   private
